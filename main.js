6c50c385e5143d6ed30812fb86a309400f5d2529
Alan Leverenz <awleverenz@aol.com>
2020-06-27 15:23:30 -0400

    Filter function

--- main.js
+++ main.js
@@ -12,8 +12,36 @@ const companies = [
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
-// forEach
-// filter
-//map
-//sort
-//reduce
+// for (let i = 0; i < companies.length; i++) {
+//   console.log(companies[i]);
+// }
+
+// forEach *****
+
+// companies.forEach(function (company) {
+//   console.log(company.name);
+// });
+
+// filter *****
+
+// let canDrink = [];
+// for (let i = 0; i < ages.length; i++) {
+//   if (ages[i] >= 21) {
+//     canDrink.push(ages[i]);
+//   }
+// }
+// console.log(canDrink);
+
+// const canDrink = ages.filter(function (age) {
+//   if (age >= 21) {
+//     return true;
+//   }
+// });
+// console.log(canDrink);
+
+const canDrink = ages.filter((age) => age >= 21);
+console.log(canDrink);
+
+//map *****
+//sort *****
+//reduce *****
